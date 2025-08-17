9f7acdb5ca94880b632c629ee23c05cb4e4ee7dc
Alp Eren <erenalpakbas@gmail.com>
2025-08-17 17:24:51 +0300

    The movies and TV shows we have uploaded are now available for viewing, and new pages have been developed.

--- MainPage/urls.py
+++ MainPage/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     #path('home/', views.index, name='index'),
     path('movies/', views.movies, name='movies'),
     path('series/', views.series, name='series'),
+    path('movie/<int:movie_id>/', views.movie_detail, name='movie_detail'),
+    path('series/<int:series_id>/', views.series_detail, name='series_detail'),
+    path('episode/<int:episode_id>/', views.episode_detail, name='episode_detail'),
 ]
--- MainPage/views.py
+++ MainPage/views.py
@@ -1,10 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Movie, Series, Episode
 
 def index(request):
-    return render(request, 'index.html')
+    movies = Movie.objects.all()[:6]
+    series = Series.objects.all()[:6]
+    context =  {
+        'movies' : movies,
+        'series' : series
+    }
+    return render(request, 'index.html', context)
+
 
 def movies(request):
-    return render(request, 'movies.html')
+    movies = Movie.objects.all()
+    return render(request, 'movies.html', {'movies':movies})
 
 def series(request):
-    return render(request, 'series.html')
+    series = Series.objects.all()
+    return render(request, 'series.html', {'series':series})
+
+
+def movie_detail(request, movie_id):
+    movie = get_object_or_404(Movie, id=movie_id)
+    return render(request, 'movie_detail.html',{'movie':movie})
+
+def series_detail(request, series_id):
+    series = get_object_or_404(Series, id=series_id)
+    episodes = series.episodes.all().order_by('season_number', 'episode_number')  # episodes_number değil episode_number!
+    return render (request, 'series_detail.html', {'series': series, 'episodes':episodes})
+
+def episode_detail(request, episode_id):
+    episode = get_object_or_404(Episode, id=episode_id)
+    other_episodes = episode.series.episodes.all().order_by('season_number', 'episode_number')
+    return render(request, 'episode_detail.html', {
+        'episode' : episode,
+        'series' : episode.series,
+        'other_episodes' : other_episodes
+    })
